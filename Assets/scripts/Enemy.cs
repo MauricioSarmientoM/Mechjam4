16feda7cf9892f33e5c962a03a855134068e3017
PaladynoSM <paladyno.sm@gmail.com>
2023-07-14 23:43:38 -0400

    Rotación enemigos y arma aniadida

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     public Rigidbody rigid;
     public float speed;
     public float maxSpeed;
-
+    public int life;
     void FixedUpdate() 
     {
         Vector3 direction = Player.player.transform.position - transform.position;
@@ -16,5 +16,17 @@ public class Enemy : MonoBehaviour
         rigid.velocity = new(Mathf.Clamp(rigid.velocity.x + direction.x, -maxSpeed, maxSpeed),
                              Mathf.Clamp(rigid.velocity.y + direction.y, -maxSpeed, maxSpeed),
                              Mathf.Clamp(rigid.velocity.z + direction.z, -maxSpeed, maxSpeed));
+
+        transform.LookAt(Player.player.transform);
+    }
+
+    public void takeDamage(int damage)
+    {
+        life = life - damage;
+
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
